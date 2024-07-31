9f2a39c339d389dbf026e36b46b073e74b39152d
CURSOTEC2024 <asaheld8@gmail.com>
2024-07-31 00:12:06 -0600

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_NOT (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,15 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+ // All output pins must be assigned. If not used, assign to 0.
+  C_NOT C1 (.a(ui_in[0]), .c(uo_out[0]));
+    assign uo_out [7:1] = 7'b0;
+    
+    assign uio_out [7:0] = 8'b0;
+    assign uio_oe [7:0] = 8'b0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire _unused = &{ena, clk, rst_n, ui_in [7:1], uio_in [7:0], 1'b0};
+
 
 endmodule
